c34a21f3b98cf947837150fe118974d7fb49156b
Drunck <nur.kons1977@gmail.com>
2022-10-31 20:10:14 +0600

    4-6.3 chapters

---
+++ cmd/web/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"snippetbox.nurkuisa.net/internal/models"
+)
+
+type application struct {
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
+}
+
+var ctx = context.Background()
+
+func main() {
+	var err error
+
+	addr := flag.String("addr", "localhost:4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://web:789@localhost:5432/snippetbox", "Postgres data source name")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	conn, err := openDB(*dsn)
+
+	if err != nil {
+		errorLog.Fatalf("\nUnable to connection to database: %v\n", err)
+	}
+	defer conn.Close()
+
+	infoLog.Println("Connected!")
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &models.SnippetModel{
+			DB: conn,
+		},
+		templateCache: templateCache,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(ctx, dsn)
+
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return db, err
+}
---
+++ internal/models/snippet.go
@@ -0,0 +1,93 @@
+package models
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *pgxpool.Pool
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	query := `INSERT INTO snippets (title, content, created, expires) 
+				VALUES($1, $2, NOW(), (NOW() + INTERVAL '1 DAY' * $3)) RETURNING id`
+
+	row := m.DB.QueryRow(context.Background(), query, title, content, expires)
+
+	var id uint64
+
+	err := row.Scan(&id)
+	if err != nil {
+		fmt.Printf("Unable to INSERT: %v\n", err)
+		return 0, nil
+	}
+
+	fmt.Println(id)
+
+	return int(id), nil
+}
+
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	query := `SELECT id, title, content, created, expires FROM snippets 
+				WHERE expires > NOW() AND id = $1`
+
+	row := m.DB.QueryRow(context.Background(), query, id)
+	s := &Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
+}
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	query := `SELECT id, title, content, created, expires FROM snippets
+				WHERE expires > NOW() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(context.Background(), query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
+}
